abaefd3a0cda221297e67c19daf0cbaa61b0aba6
christinegithub <christine13@gmail.com>
2019-04-05 13:36:34 -0400

    exercise complete

--- calculator.js
+++ calculator.js
@@ -1,10 +1,17 @@
-function add() {
+function add(a , b) {
+  return a + b;
 }
 
-function subtract() {
+function subtract(a, b) {
+  return a - b;
 }
 
-function sum() {
+function sum(list) {
+  total = 0
+  for (let i = 0; i < list.length; i++) {
+    total += list[i];
+  }
+  return total;
 }
 
 module.exports = {add, subtract, sum}
